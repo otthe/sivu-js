34fbdd28548c58e83dc8829b10b64950c6d55dda
ottoheiskanen <ottoheiskanen@gmail.com>
2026-02-26 20:27:42 +0200

    starting to work on param routing and prettyfying routes

--- config.js
+++ config.js
@@ -9,4 +9,11 @@ module.exports = {
   root_file: 'index.sivu',
   force_csrf_middleware: true,
   session_secret: process.env.SESSION_SECRET || 'thisismysecret',
+  pretty_routing: true,
+  
+  routes: [
+    { method: "GET",  path: "/users/:id",      template: "user_show.sivu" },
+    { method: "POST", path: "/users/:id",      action: "_user_update.sivu" },
+    { method: "GET",  path: "/posts/:slug",    template: "post.sivu" },
+  ]
 };
